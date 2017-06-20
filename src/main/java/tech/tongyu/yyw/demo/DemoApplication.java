8161636162e76064e9553b8e36cb1118fbe09098
David Yang <David Yang>
2017-06-20 23:07:11 +0800

    Initial commit

---
+++ src/main/java/tech/tongyu/yyw/demo/DemoApplication.java
@@ -0,0 +1,10 @@
+package tech.tongyu.yyw.demo;
+
+/**
+ * Created by yangyiwei on 2017/6/20.
+ */
+public class DemoApplication {
+    public void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
